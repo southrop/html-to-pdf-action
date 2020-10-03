7d78b8a6a8014e395e0ec692c976a37685058512
southrop <southrop113@gmail.com>
2020-10-04 08:52:05 +0900

    Try implementing pdf conversion

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,67 @@
 import * as core from '@actions/core'
+import { Server } from './server'
+import PDFMerger from 'pdf-merger-js'
+import * as puppeteer from 'puppeteer'
+import { PORT } from './constants'
 
 async function run(): Promise<void> {
-    const inputPaths: string = core.getInput('inputPaths')
-    const outputPath: string = core.getInput('outputPath')
-    const options: string = core.getInput('puppeteerOptions')
+    try {
+        const inputPaths: string = core.getInput('inputPaths')
+        let outputPath: string = core.getInput('outputPath')
+        const options: string = core.getInput('puppeteerOptions')
 
-    console.log('inputPaths: ' + inputPaths)
-    console.log('outputPath: ' + outputPath)
-    console.log('options: ' + options)
+        console.log('inputPaths: ' + inputPaths)
+        console.log('outputPath: ' + outputPath)
+        console.log('options: ' + options)
 
-    const optionsObj = JSON.parse(options)
-    console.log('optionsObj: ' + JSON.stringify(optionsObj, null, 4))
+        const pdfOptions = JSON.parse(options)
+        console.log('optionsObj: ' + JSON.stringify(pdfOptions, null, 4))
 
-    const inputArray = inputPaths.split(",")
-    inputArray.forEach(input => {
-        console.log('Input: ' + input)
-    })
+        const browser = await puppeteer.launch()
+        const tab = await browser.newPage()
+        const merger = new PDFMerger()
+
+        const inputArray = inputPaths.split(',')
+
+        let isLocal = false
+        inputArray.some(input => {
+            if (!input.startsWith('http')) {
+                isLocal = true
+                return true
+            }
+        })
+        let server: Server | undefined
+        if (isLocal) {
+            server = new Server(process.cwd()) // start local server
+        }
+
+        inputArray.forEach(async (input, index) => {
+            console.log(`Printing page ${index}: ${input}`)
+
+            const pageUrl = input.startsWith('http')
+                ? input
+                : `http://localhost:${PORT}/${input}`
+            await tab.goto(pageUrl, { waitUntil: 'networkidle0' })
+
+            const pageOptions = { ...pdfOptions, path: `./page${index}.pdf` }
+            await tab.pdf(pageOptions)
+
+            merger.add(`page${index}.pdf`)
+        })
+
+        if (server !== undefined) {
+            server.close()
+        }
+        await browser.close()
+
+        if (!outputPath.endsWith('.pdf')) {
+            outputPath += '.pdf' // append file extension
+        }
+
+        await merger.save(outputPath)
+    } catch (error) {
+        core.error(error)
+    }
 }
 
 run()
---
+++ src/server.ts
@@ -0,0 +1,62 @@
+import fs from 'fs'
+import http from 'http'
+import path from 'path'
+import { PORT } from './constants'
+
+export class Server {
+    server: http.Server
+    mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.svg': 'image/svg+xml',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.wasm': 'application/wasm'
+    }
+
+    constructor(rootPath: string) {
+        this.server = http
+            .createServer((request, response) => {
+                console.log('Request: ' + request.url)
+
+                let filePath = path.join(rootPath, request.url || '')
+                if (request.url === '/') {
+                    filePath = path.join(rootPath, 'index.html')
+                }
+
+                const extname = String(path.extname(filePath)).toLowerCase()
+                const contentType =
+                    this.mimeTypes[extname] || 'application/octet-stream'
+
+                fs.readFile(filePath, (error, content) => {
+                    if (error) {
+                        console.log('Error: ' + error)
+                        if (error.code === 'ENOENT') {
+                            response.writeHead(404)
+                            response.end('404')
+                        } else {
+                            response.writeHead(500)
+                            response.end(error.code)
+                        }
+                    } else {
+                        response.writeHead(200, { 'Content-Type': contentType })
+                        response.end(content, 'utf-8')
+                    }
+                })
+            })
+            .listen(PORT)
+    }
+
+    close(): void {
+        this.server.close()
+    }
+}
